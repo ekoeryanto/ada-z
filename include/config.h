13e8f4f0e9093ed0c232e08010d360fbfdc079c4
Eko Eryanto <ekoeryanto@gmail.com>
2025-10-08 00:55:50 +0700

    config: disable verbose logging for production

--- include/config.h
+++ include/config.h
@@ -46,7 +46,7 @@ static const char* const PREFERRED_PASSES[] = {
 
 // Logging verbosity
 #ifndef ENABLE_VERBOSE_LOGS
-#define ENABLE_VERBOSE_LOGS 1  // Temporarily enabled for SD card debugging
+#define ENABLE_VERBOSE_LOGS 0  // Set to 1 for debugging SD card issues
 #endif
 
 #if ENABLE_VERBOSE_LOGS
